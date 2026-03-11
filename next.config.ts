ee677da76eb747919212f6ef9006ae0908dce9eb
Bramantyo Aryo <bram.antyo2796@gmail.com>
2026-03-11 20:20:09 +0700

    router config ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: '/graphql',
+        destination: process.env.GRAPHQL_URL || 'http://nginx-server:80/graphql',
+      },
+    ];
+  },
 };
 
 export default nextConfig;
